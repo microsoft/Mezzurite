c12ebc1f9c387b559ea747a869b229a405cb6e49
Brent Rusinow <brrusino@microsoft.com>
2018-10-25 10:44:30 -0700

    fixed to match documentation better

--- Mezzurite.Angular/src/index.ts
+++ Mezzurite.Angular/src/index.ts
@@ -1,7 +1,1 @@
 ﻿export * from "./angular-performance.module";
-export * from "./routing.service";
-
-export const environment = {
-        version: require('../package.json').version,
-        name: require('../package.json').name
-    };
