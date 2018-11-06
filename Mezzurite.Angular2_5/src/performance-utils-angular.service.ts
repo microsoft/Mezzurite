be2d7b942cf43e7b38c1d61f5c90e26857705e9d
Brent Rusinow <brrusino@microsoft.com>
2018-11-06 15:52:39 -0800

    minor changes to Angular utils

--- Mezzurite.Angular2_5/src/performance-utils-angular.service.ts
+++ Mezzurite.Angular2_5/src/performance-utils-angular.service.ts
@@ -13,12 +13,4 @@ export class MezzuriteAngularUtils extends MezzuriteUtils{
         (<any>window).mezzurite.packageVersion = environment.version;
         (<any>window).mezzurite.packageName = environment.name;
     }
-
-    static getName(name: string, key: string, clarifier: string = null): string{
-       return super.getName(name, key, clarifier);
-    }
-
-    static makeId(): string{
-        return super.makeId();
-    }
 }
