e631a1434b7690f582c64ea12376efbb95994fd5
Brent Rusinow <brrusino@microsoft.com>
2018-11-30 10:09:40 -0800

    refactoring for AOT support

---
+++ Mezzurite.Angular/src/mezzurite-angular.ts
@@ -0,0 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+export { RoutingService } from './services/angular-routing.service';
+export { AngularPerfModule } from './modules/angular-performance.module';
---
+++ Mezzurite.Angular/src/modules/angular-performance.module.ts
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import { NgModule, ModuleWithProviders } from "@angular/core";
+import { MezzuriteDirective } from '../components/angular-performance.directive';
+import { RoutingService } from "../services/angular-routing.service";
+
+
+@NgModule({
+    declarations: [MezzuriteDirective],
+    exports: [MezzuriteDirective]
+})
+
+/**
+ * Mezzurite Performance Module for Angular
+ */
+export class AngularPerfModule {
+    static forRoot() : ModuleWithProviders {
+        return {
+            ngModule: AngularPerfModule,
+            providers: [
+                RoutingService
+                // Add new services here.
+            ]
+        };
+    }
+}
