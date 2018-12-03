105e057ca16c6d2f134ea878a9cf9e2968f0c7fc
Brent Rusinow <brrusino@microsoft.com>
2018-12-03 10:43:23 -0800

    more package updates

--- Mezzurite.Core/src/mezzurite-core.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-export * from "./services/performance-telemetry.service";
-export * from "./services/performance-timing.service";
-export * from "./utils/performance-metric";
-export * from "./utils/performance-constants";
-export * from "./services/performance-utils.service"
