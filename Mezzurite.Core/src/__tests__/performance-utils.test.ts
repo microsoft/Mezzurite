a44b66f92f841391473a5059a578d13b28852d4c
Brent Rusinow <brrusino@microsoft.com>
2018-12-07 10:54:30 -0800

    telemetry object refactor, linted core library

--- Mezzurite.Core/src/__tests__/performance-utils.test.ts
+++ Mezzurite.Core/src/__tests__/performance-utils.test.ts
@@ -4,26 +4,26 @@
 import {MezzuriteUtils} from '../services/performance-utils.service';
 import {MezzuriteConstants} from '../utils/performance-constants';
 
-function myTestFunction(){
-  console.log("this is a test!");
+function myTestFunction() {
+  console.log('this is a test!');
 }
 
-describe("Mezzurite Utils Tests:", () => {
-  it('Get function name works as expected', () => {  
-    expect(MezzuriteUtils.getFunctionName(myTestFunction)).toBe("myTestFunction");
+describe('Mezzurite Utils Tests:', () => {
+  it('Get function name works as expected', () => {
+    expect(MezzuriteUtils.getFunctionName(myTestFunction)).toBe('myTestFunction');
   });
-  
-  it('Get name works as expected', () => {  
-    expect(MezzuriteUtils.getName("myComponent", "abc123")).toBe(MezzuriteConstants.measureNamePrefix + ";myComponent;abc123");
+
+  it('Get name works as expected', () => {
+    expect(MezzuriteUtils.getName('myComponent', 'abc123')).toBe(MezzuriteConstants.measureNamePrefix + ';myComponent;abc123');
   });
-  
-  it('Get full name part works as expected', () => {  
-    const fullName = "mz;myComponent;abc123";
-    expect(MezzuriteUtils.getFullNamePart(fullName, MezzuriteConstants.fullNamePartKey)).toBe("abc123");
-    expect(MezzuriteUtils.getFullNamePart(fullName, MezzuriteConstants.fullNamePartTitle)).toBe("myComponent");
-    expect(MezzuriteUtils.getFullNamePart(fullName, "pizza")).toBe(fullName);
+
+  it('Get full name part works as expected', () => {
+    const fullName = 'mz;myComponent;abc123';
+    expect(MezzuriteUtils.getFullNamePart(fullName, MezzuriteConstants.fullNamePartKey)).toBe('abc123');
+    expect(MezzuriteUtils.getFullNamePart(fullName, MezzuriteConstants.fullNamePartTitle)).toBe('myComponent');
+    expect(MezzuriteUtils.getFullNamePart(fullName, 'pizza')).toBe(fullName);
   });
-})
+});
 
 
 
--- Mezzurite.Core/src/services/performance-timing.service.ts
+++ Mezzurite.Core/src/services/performance-timing.service.ts
@@ -14,68 +14,66 @@ export class PerformanceTimingService {
      * @param slowestResource Slowest resource inside component
      * @param maxComponentEndTime Max component end time (if VLT)
      */
-    static measure(name: string, slowestResource: any = null, maxComponentEndTime: any = null): void{
+    static measure(name: string, slowestResource: any = null, maxComponentEndTime: any = null): void {
         let startEntry;
         let endEntry;
 
         const componentTitle = MezzuriteUtils.getFullNamePart(name, MezzuriteConstants.fullNamePartTitle);
- 
+
         const key = MezzuriteUtils.getFullNamePart(name, MezzuriteConstants.fullNamePartKey);
-        if (name === undefined){
+        if (name === undefined) {
             return;
         }
-    
-        if (componentTitle === MezzuriteConstants.altName){
+        if (componentTitle === MezzuriteConstants.altName) {
             // ALT
             endEntry = performance.getEntriesByName(MezzuriteConstants.altMarkEnd)[0];
-        }
-        else if (componentTitle === MezzuriteConstants.vltName){
+        } else if (componentTitle === MezzuriteConstants.vltName) {
             // VLT
             startEntry = performance.getEntriesByName(MezzuriteConstants.vltMarkStart)[0];
             endEntry = {
                 startTime: maxComponentEndTime
             };
-        }
-        else{
+        } else {
             // Component
             startEntry = performance.getEntriesByName(key + MezzuriteConstants.componentMarkStart)[0];
             endEntry = performance.getEntriesByName(key + MezzuriteConstants.componentMarkEnd)[0];
         }
         // start time inside render hook
         const renderStartEntry = performance.getEntriesByName(key + MezzuriteConstants.componentMarkRenderStart)[0];
-    
         const startTime = startEntry !== undefined ? startEntry.startTime : 0;
-        let endTime = endEntry.startTime;
-    
-        // reset end time to end of slowest resource if 
-        if (slowestResource !== null && slowestResource.responseEnd > endTime){
+        let endTime: number = endEntry.startTime;
+
+        // reset end time to end of slowest resource if
+        if (slowestResource !== null && slowestResource.responseEnd > endTime) {
             endTime = slowestResource.responseEnd;
         }
-    
+
         const mountDuration = endEntry.startTime - startTime;
-        const totalDuration = endTime - startTime
+        const totalDuration = endTime - startTime;
         const nameArr = name.split(';');
-    
-        let obj = {
+
+        const obj = {
             name: nameArr[1],
             id: nameArr[2],
-            startTime: startTime,
-            endTime: endTime,
-            timeToMount: mountDuration,
-            componentLoadTime: totalDuration
+            startTime: startTime % 1 !== 0 ? parseFloat(startTime.toFixed(1)) : startTime,
+            endTime: parseFloat(endTime.toFixed(1)),
+            untilMount: parseFloat(mountDuration.toFixed(1)),
+            clt: parseFloat(totalDuration.toFixed(1)),
+            slowResource: {}
+        };
+
+        if (slowestResource && slowestResource.responseEnd >= startTime) {
+            (<any>obj).slowResource['end'] = parseFloat(slowestResource.responseEnd.toFixed(1));
+            (<any>obj).slowResource['name'] = slowestResource.name;
+        } else if (slowestResource !== null) {
+            (<any>obj).slowResource['end'] = -1;
+            (<any>obj).slowResource['name'] = slowestResource.name;
         }
-    
-        if (slowestResource && slowestResource.responseEnd >= startTime){
-            (<any>obj)["slowestResourceEnd"] = slowestResource.responseEnd;
+
+        if (componentTitle !== MezzuriteConstants.altName && componentTitle !== MezzuriteConstants.vltName && renderStartEntry) {
+            (<any>obj)['renderStartTime'] = renderStartEntry.startTime;
         }
-        else if (slowestResource !== null) {
-            (<any>obj)["slowestResourceEnd"] = -1;
-        }
-    
-        if (componentTitle !== MezzuriteConstants.altName && componentTitle !== MezzuriteConstants.vltName && renderStartEntry){
-            (<any>obj)["renderStartTime"] = renderStartEntry.startTime;
-        }
-    
+
         (<any>window).mezzurite.measures.push(obj);
     }
 
@@ -83,14 +81,14 @@ export class PerformanceTimingService {
      * Gets measures by name
      * @param name name
      */
-    static getMeasuresByName(name: string){
-        let result: any[] = [];
-        if (name === undefined || name === null){
+    static getMeasuresByName(name: string) {
+        const result: any[] = [];
+        if (name === undefined || name === null) {
             return null;
         }
         const measures = (<any>window).mezzurite.measures;
-        for (let i = 0; i < measures.length; i++){
-            if (name === measures[i].name){
+        for (let i = 0; i < measures.length; i++) {
+            if (name === measures[i].name) {
                 result.push(measures[i]);
             }
         }
@@ -101,13 +99,13 @@ export class PerformanceTimingService {
      * Gets a specific measure by id
      * @param id id
      */
-    static getMeasureById(id: number){
-        if (id === undefined || id === null){
+    static getMeasureById(id: number) {
+        if (id === undefined || id === null) {
             return null;
         }
         const measures = (<any>window).mezzurite.measures;
-        for (let i = 0; i < measures.length; i++){
-            if (id === measures[i].id){
+        for (let i = 0; i < measures.length; i++) {
+            if (id === measures[i].id) {
                 return measures[i];
             }
         }
@@ -119,13 +117,13 @@ export class PerformanceTimingService {
      * @param name name
      * @param id id
      */
-    static getMeasureByNameAndId(name: string, id: number){
-        if (name === undefined || name === null || id === undefined || id === null){
+    static getMeasureByNameAndId(name: string, id: number) {
+        if (name === undefined || name === null || id === undefined || id === null) {
             return null;
         }
         const measures = (<any>window).mezzurite.measures;
-        for (let i = 0; i < measures.length; i++){
-            if (name === measures[i].name && id === measures[i].id){
+        for (let i = 0; i < measures.length; i++) {
+            if (name === measures[i].name && id === measures[i].id) {
                 return measures[i];
             }
         }
@@ -135,28 +133,28 @@ export class PerformanceTimingService {
     /**
      * Gets current components from a given capture cycle
      */
-    static getCurrentComponents(){
-        const components = (<any>window).mezzurite.measures.filter((m:any) => 
-            m.name.indexOf(MezzuriteConstants.measureNamePrefix + ";" + MezzuriteConstants.altName) === -1 &&
-            m.name.indexOf(MezzuriteConstants.measureNamePrefix + ";" + MezzuriteConstants.vltName) === -1 &&
+    static getCurrentComponents() {
+        const components = (<any>window).mezzurite.measures.filter((m: any) =>
+            m.name.indexOf(MezzuriteConstants.measureNamePrefix + ';' + MezzuriteConstants.altName) === -1 &&
+            m.name.indexOf(MezzuriteConstants.measureNamePrefix + ';' + MezzuriteConstants.vltName) === -1 &&
             m.startTime >= (<any>window).mezzurite.startTime &&
             m.startTime <= (<any>window).mezzurite.endTime
-        )
+        );
         return components;
     }
 
     /**
      * Gets lookup object of current components
      */
-    static getCurrentComponentsLookup(){
-        const components = (<any>window).mezzurite.measures.filter((m:any) => 
-            m.name.indexOf(MezzuriteConstants.measureNamePrefix + ";" + MezzuriteConstants.altName) === -1 &&
-            m.name.indexOf(MezzuriteConstants.measureNamePrefix + ";" + MezzuriteConstants.vltName) === -1 &&
+    static getCurrentComponentsLookup() {
+        const components = (<any>window).mezzurite.measures.filter((m: any) =>
+            m.name.indexOf(MezzuriteConstants.measureNamePrefix + ';' + MezzuriteConstants.altName) === -1 &&
+            m.name.indexOf(MezzuriteConstants.measureNamePrefix + ';' + MezzuriteConstants.vltName) === -1 &&
             m.startTime >= (<any>window).mezzurite.startTime &&
             m.startTime <= (<any>window).mezzurite.endTime
-        )
-        let obj: any = {};
-        for (let i = 0; i < components.length; i++){
+        );
+        const obj: any = {};
+        for (let i = 0; i < components.length; i++) {
             obj[MezzuriteConstants.measureNamePrefix + ';' + components[i].name + ';' + components[i].id] = components[i];
         }
         return obj;
@@ -165,60 +163,58 @@ export class PerformanceTimingService {
     /**
      * Calculates viewport load time
      */
-    static calculateVlt(){
+    static calculateVlt() {
         let maxComponent = null;
         let maxEndTime = 0;
-        let vltComponents: any[] = [];
+        const vltComponents: any[] = [];
         const components: any = this.getCurrentComponentsLookup();
-        let vltLookup = (<any>window).mezzurite.vltComponentLookup;
+        const vltLookup = (<any>window).mezzurite.vltComponentLookup;
         let measure;
-        for (let key in vltLookup){
-            if (components[key] && vltLookup[key] === true){
+        for (const key in vltLookup) {
+            if (components[key] && vltLookup[key] === true) {
                 vltComponents.push(components[key]);
-                if (maxComponent !== null){
+                if (maxComponent !== null) {
                     let slowestResourceEnd = 0;
                     const slowestResource = (<any>window).mezzurite.slowestResource[key];
-                    if (slowestResource !== undefined && slowestResource !== null){
+                    if (slowestResource !== undefined && slowestResource !== null) {
                         slowestResourceEnd = slowestResource.responseEnd;
                     }
-                    var maxLast = maxComponent.componentLoadTime + maxComponent.startTime;
-                    var currLast = components[key].componentLoadTime + components[key].startTime;
-                    if (currLast > maxLast){
+                    const maxLast = maxComponent.componentLoadTime + maxComponent.startTime;
+                    const currLast = components[key].componentLoadTime + components[key].startTime;
+                    if (currLast > maxLast) {
                         maxComponent = components[key];
                         maxEndTime = currLast;
                     }
-                }
-                else {
+                } else {
                     maxComponent = components[key];
                 }
             }
         }
-        if (maxComponent !== null){
+        if (maxComponent !== null) {
             const fullName = MezzuriteConstants.measureNamePrefix + ';' + MezzuriteConstants.vltName + ';' + maxComponent.id;
             this.measure(fullName, null, maxComponent.endTime);
             measure = this.getMeasureByNameAndId(MezzuriteConstants.vltName, maxComponent.id);
             performance.clearMarks(MezzuriteConstants.vltMarkStart);
-        }
-        else {
+        } else {
             return null;
             // no components in view
         }
         return {
-            vlt: measure.componentLoadTime,
+            vlt: measure.clt,
             components: vltComponents
-        }
+        };
     }
 
     /**
      * Creates sub-element lookup object on global mezzurite object
-     * @param el 
-     * @param key 
+     * @param el
+     * @param key
      */
-    static getElNames(el: any, key: string){
-        if ((<any>window).mezzurite.childElementNames[key] === undefined){
+    static getElNames(el: any, key: string) {
+        if ((<any>window).mezzurite.childElementNames[key] === undefined) {
             (<any>window).mezzurite.childElementNames[key] = [];
         }
-        if (el.tagName === 'IMG'){
+        if (el.tagName === 'IMG') {
             (<any>window).mezzurite.childElementNames[key].push(el.src);
         }
     }
@@ -228,36 +224,37 @@ export class PerformanceTimingService {
      * @param el parent element
      * @param fullName component fullname
      */
-    static calculateSlowestResource(el: any, fullName: string){
+    static calculateSlowestResource(el: any, fullName: string) {
         const key = MezzuriteUtils.getFullNamePart(fullName, MezzuriteConstants.fullNamePartKey);
         let slowestResource = null;
         MezzuriteUtils.walkDOM(el, key, this.getElNames);
-    
-        const resources: any = performance.getEntriesByType("resource").filter((r:any) => (<any>r).initiatorType === 'img');
+
+        const resources: any = performance.getEntriesByType('resource').filter((r: any) => (<any>r).initiatorType === 'img');
         const currentResources = (<any>window).mezzurite.childElementNames[key];
-        if (resources.length === 0){
+        if (resources.length === 0) {
             return;
         }
-        for (let i = 0; i < currentResources.length; i++){
-            for (let j = 0; j < resources.length; j++){
-                if (currentResources[i] === resources[j].name && (slowestResource === null || resources[j].responseEnd > slowestResource.responseEnd)){
-                    slowestResource = resources[j];
+        for (let i = 0; i < currentResources.length; i++) {
+            for (let j = 0; j < resources.length; j++) {
+                if (currentResources[i] === resources[j].name &&
+                   (slowestResource === null || resources[j].responseEnd > slowestResource.responseEnd)) {
+                        slowestResource = resources[j];
                 }
             }
         }
         (<any>window).mezzurite.slowestResource[fullName] = slowestResource;
         return slowestResource;
-    }   
+    }
 
-    static calculateSlowestResourceBatch(){
+    static calculateSlowestResourceBatch() {
+        let slow;
         const elementDict = (<any>window).mezzurite.elementLookup;
-        for (let prop in elementDict){
-            let slow = this.calculateSlowestResource(elementDict[prop], prop);
-            if (slow === null){
-                PerformanceTimingService.measure(prop)
-            }
-            else{
-                PerformanceTimingService.measure(prop, slow)
+        for (const prop in elementDict) {
+            slow = this.calculateSlowestResource(elementDict[prop], prop);
+            if (slow === null) {
+                PerformanceTimingService.measure(prop);
+            } else {
+                PerformanceTimingService.measure(prop, slow);
             }
         }
     }
