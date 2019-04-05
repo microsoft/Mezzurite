38daf1c09afebfe2759e6f6d1ed6846fb7b808f6
Ka-wai Wong <Kawai.Wong@Microsoft.com>
2019-04-04 19:15:46 -0700

    Install babel, eslint, jest and rollup

---
+++ rollup.config.js
@@ -0,0 +1,117 @@
+import babel from 'rollup-plugin-babel'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import replace from 'rollup-plugin-replace'
+import { terser } from 'rollup-plugin-terser'
+
+import pkg from './package.json'
+
+export default [
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'lib/mezzurite.js',
+      format: 'cjs',
+      indent: false
+    },
+    external: [
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {})
+    ],
+    plugins: [
+      nodeResolve({
+        jsnext: true
+      }),
+      babel()
+    ]
+  },
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'es/mezzurite.js',
+      format: 'es',
+      indent: false
+    },
+    external: [
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {})
+    ],
+    plugins: [
+      nodeResolve({
+        jsnext: true
+      }),
+      babel()
+    ]
+  },
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'es/mezzurite.mjs',
+      format: 'es',
+      indent: false
+    },
+    plugins: [
+      nodeResolve({
+        jsnext: true
+      }),
+      replace({
+        'process.env.NODE_ENV': JSON.stringify('production')
+      }),
+      terser({
+        compress: {
+          pure_getters: true,
+          unsafe: true,
+          unsafe_comps: true,
+          warnings: false
+        }
+      })
+    ]
+  },
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/mezzurite.js',
+      format: 'umd',
+      name: 'Mezzurite',
+      indent: false
+    },
+    plugins: [
+      nodeResolve({
+        jsnext: true
+      }),
+      babel({
+        exclude: 'node_modules/**'
+      }),
+      replace({
+        'process.env.NODE_ENV': JSON.stringify('development')
+      })
+    ]
+  },
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/mezzurite.min.js',
+      format: 'umd',
+      name: 'Mezzurite',
+      indent: false
+    },
+    plugins: [
+      nodeResolve({
+        jsnext: true
+      }),
+      babel({
+        exclude: 'node_modules/**'
+      }),
+      replace({
+        'process.env.NODE_ENV': JSON.stringify('production')
+      }),
+      terser({
+        compress: {
+          pure_getters: true,
+          unsafe: true,
+          unsafe_comps: true,
+          warnings: false
+        }
+      })
+    ]
+  }
+]
