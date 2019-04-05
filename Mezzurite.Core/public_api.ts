3eb033f2517f55f3974a5ea142a2d142b61cfa4b
Ka-wai Wong <Kawai.Wong@Microsoft.com>
2019-04-04 19:10:47 -0700

    Remove previous implementation

--- Mezzurite.Core/public_api.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-/**
- * Entry point for all public APIs of the package.
- */
-export * from './src/mezzurite-core';
-
-// This file only reexports content of the `src` folder. Keep it that way.
