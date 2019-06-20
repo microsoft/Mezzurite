4f74de66266d2f0bb4be01ef891ec3c802834e9f
Ka-wai Wong <Kawai.Wong@Microsoft.com>
2019-06-20 13:49:37 -0700

    Add redux store and components reducer

---
+++ src/actions/components.js
@@ -0,0 +1,14 @@
+const componentEnd = (component) => ({
+  type: 'COMPONENT_END',
+  payload: component
+});
+
+const componentStart = (component) => ({
+  type: 'COMPONENT_START',
+  payload: component
+});
+
+export {
+  componentEnd,
+  componentStart
+};
---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+
+import components from './components';
+
+export default combineReducers({
+  components
+});
