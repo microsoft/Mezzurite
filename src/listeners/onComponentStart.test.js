dc7163592765e5caa05101451b7bb4f514dea5ca
C. Naoto Abreu Takemura <caabreut@microsoft.com>
2019-07-15 12:32:53 -0700

    Fixed whitespace ESLint warnings.

--- src/listeners/onComponentStart.test.js
+++ src/listeners/onComponentStart.test.js
@@ -19,47 +19,47 @@ describe('onComponentStart.js', () => {
   describe('invalid events', () => {
     it('should not dispatch when the event is null', () => {
       onComponentStart(null);
-  
+
       expect(store.dispatch).not.toHaveBeenCalled();
     });
-  
+
     it('should not dispatch when the event is undefined', () => {
       onComponentStart(undefined);
-  
+
       expect(store.dispatch).not.toHaveBeenCalled();
     });
-  
+
     it('should not dispatch when the event detail is null', () => {
       onComponentStart({
         detail: null
       });
-  
+
       expect(store.dispatch).not.toHaveBeenCalled();
     });
-  
+
     it('should not dispatch when the event detail is undefined', () => {
       onComponentStart({
         detail: undefined
       });
-  
+
       expect(store.dispatch).not.toHaveBeenCalled();
     });
   });
 
   describe('valid events', () => {
     it('should not dispatch when the component id already exists in the store', () => {
-      store.getState = jest.fn(() => ({'id': {}}));
+      store.getState = jest.fn(() => ({ 'id': {} }));
       onComponentStart({
         detail: {
           id: 'id',
           name: 'name'
         }
       });
-      
+
       expect(store.dispatch).not.toHaveBeenCalled();
       expect(spyWarn).toHaveBeenCalled();
     });
-  
+
     it('should dispatch the component data to the store', () => {
       performance.now = jest.fn(() => 5);
       onComponentStart({
@@ -68,7 +68,7 @@ describe('onComponentStart.js', () => {
           name: 'name'
         }
       });
-  
+
       expect(store.dispatch).toHaveBeenCalledWith(componentStart({
         id: 'id',
         name: 'name',
