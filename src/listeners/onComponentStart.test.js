bb0380f03ae573715671dff9756ce98bab144382
C. Naoto Abreu Takemura <caabreut@microsoft.com>
2019-07-15 11:50:26 -0700

    Add unit test case for onComponentStart: it should not dispatch if the component id already exists in the store

--- src/listeners/onComponentStart.test.js
+++ src/listeners/onComponentStart.test.js
@@ -6,6 +6,7 @@ describe('onComponentStart.js', () => {
   beforeEach(() => {
     jest.clearAllMocks();
     store.dispatch = jest.fn();
+    store.getState = jest.fn(() => ({}));
   });
 
   it('should not dispatch when the event is null', () => {
@@ -31,6 +32,18 @@ describe('onComponentStart.js', () => {
   it('should not dispatch when the event detail is undefined', () => {
     onComponentStart({
       detail: undefined
+    });
+
+    expect(store.dispatch).not.toHaveBeenCalled();
+  });
+
+  it('should not dispatch when the component id already exists in the store', () => {
+    store.getState = jest.fn(() => ({'id': {}}));
+    onComponentStart({
+      detail: {
+        id: 'id',
+        name: 'name'
+      }
     });
 
     expect(store.dispatch).not.toHaveBeenCalled();
